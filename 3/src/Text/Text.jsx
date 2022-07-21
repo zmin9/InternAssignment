6e0294a65574584b8ff212d367c045f662be8a4e
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-21 11:45:55 +0900

    feat: #3 set structure using Stack Component

--- 3/src/Text/Text.jsx
+++ 3/src/Text/Text.jsx
@@ -4,12 +4,13 @@ const TextElement = styled.span`
   font-size: ${props => props.size};
   font-weight: ${props => props.weight};
   color: ${props => props.color};
+  line-height: ${props => props.lineHeight || 1};
   font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', sans-serif;
 `;
 
-const Text = ({ children, size, weight, color }) => {
+const Text = ({ children, size, weight, color, lineHeight }) => {
   return (
-    <TextElement size={size} weight={weight} color={color}>
+    <TextElement size={size} weight={weight} color={color} lineHeight={lineHeight}>
       {children}
     </TextElement>
   );
--- 3/src/mainPage/MainPage.js
+++ 3/src/mainPage/MainPage.js
@@ -4,11 +4,26 @@ import Stack from "../Stack/Stack";
 function MainPage () {
   return (
     <>
-      <Stack spacing="2">
-        <Text size="32px" weight="700" color="var(--black-dark)">2020년 7월 1일</Text>
-        <Text size="14px" weight="600" color="var(--black-main)">3개 진행중, 2개 완료됨</Text>
-        <Stack spacing="1">
-        
+      <Stack spacing={2}>
+        <Stack spacing={1}>
+          <Text size="32px" weight="700" color="var(--black-dark)" lineHeight="38.73px">2020년 7월 1일</Text>
+          <Text size="14px" weight="600" color="var(--black-main)">3개 진행중, 2개 완료됨</Text>
+        </Stack>
+        <hr/>
+        <Stack spacing={4}>
+          <Stack spacing={2}>
+            <Text size="18px" weight="700" color="var(--black-main)"> 진행중 </Text>
+            <Stack row spacing={2}>
+              <div>네모</div>
+              <Stack spacing={0}>
+                <Text size="14px" weight="500" color="var(--black-main)" lineHeight="24px">월급 통장 만들기</Text>
+                <Text size="14px" weight="600" color="var(--black-light)">💰 은행</Text>
+              </Stack>
+            </Stack>
+          </Stack>
+          <Stack spaing={2}>
+            <Text size="18px" weight="700" color="var(--black-main)"> 완료됨 </Text>
+          </Stack>
         </Stack>
       </Stack>
     </>
