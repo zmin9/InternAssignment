3a524606e54163ffe58bb2a8fdd5c9a2accf04f6
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-21 11:27:06 +0900

    fix: #3 fix bug applying same style even though different props

---
+++ 3/src/Stack/Stack.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const StackContainer = styled.div`
+  display: flex;
+  flex-direction: ${props => props.row? "row" : "column"};
+`;
+
+const StackItem = styled.div`
+  margin: ${props => props.row ?
+          `0 0 0 var(--spacing-${props.spacing})` : `var(--spacing-${props.spacing}) 0 0 0`
+  };
+
+  &:first-child {
+    margin: 0;
+  }
+`;
+
+const Stack = ({children, spacing, row}) => {
+  const checkIterable = (obj) => {
+    if (obj === null || obj === undefined) return false;
+    return typeof obj[Symbol.iterator] === "function";
+  }
+  return (
+    <StackContainer row={row}>
+      { checkIterable(children)?
+          children.map((child, idx) =>
+            <StackItem key={idx} spacing={spacing} row={row}>
+              {child}
+            </StackItem>
+          )
+        :
+        children
+      }
+    </StackContainer>
+  );
+};
+
+export default Stack;
--- 3/src/mainPage/MainPage.js
+++ 3/src/mainPage/MainPage.js
@@ -1,13 +1,14 @@
 import Text from "../Text/Text";
-import Stack from "../stack";
+import Stack from "../Stack/Stack";
 
 function MainPage () {
   return (
     <>
-      <Stack spacing={16}>
-        <Stack spacing={8}>
-          <Text size="32px" weight="700" color="var(--black-dark)">2020년 7월 1일</Text>
-          <Text size="14px" weight="600" color="var(--black-main)">3개 진행중, 2개 완료됨</Text>
+      <Stack spacing="2">
+        <Text size="32px" weight="700" color="var(--black-dark)">2020년 7월 1일</Text>
+        <Text size="14px" weight="600" color="var(--black-main)">3개 진행중, 2개 완료됨</Text>
+        <Stack spacing="1">
+        
         </Stack>
       </Stack>
     </>
