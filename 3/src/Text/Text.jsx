d8b5d909924a0129ff05f34f49c544434630eaf5
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-22 16:04:55 +0900

    fix: #3 font

--- 3/src/Text/Text.jsx
+++ 3/src/Text/Text.jsx
@@ -5,15 +5,6 @@ const Text = styled.span`
   font-weight: ${props => props.weight};
   color: ${props => props.color};
   line-height: ${props => props.lineHeight || 1};
-  font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', sans-serif;
 `;
-
-// const Text = ({ children, size, weight, color, lineHeight }) => {
-//   return (
-//     <TextElement size={size} weight={weight} color={color} lineHeight={lineHeight}>
-//       {children}
-//     </TextElement>
-//   );
-// };
 
 export default Text;
