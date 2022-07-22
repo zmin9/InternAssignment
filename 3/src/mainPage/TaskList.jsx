b3d532ba9ecd5c33722e58a904e9e5883f5e3a91
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-22 22:30:16 +0900

    feat: #3 add '항목이 없습니다.' msg in empty list

--- 3/src/mainPage/TaskList.jsx
+++ 3/src/mainPage/TaskList.jsx
@@ -1,14 +1,26 @@
 import TaskItem from "./TaskItem";
 import Stack from "../layout/Stack";
 import Typography from "../text/Typography";
+import styled from "styled-components";
+import Text from "../text/Text";
+
+const EmptyListMsg = styled.div`
+  padding: var(--spacing-2);
+  text-align: center;
+`;
 
 const TaskList = ({title, tasks, onClick}) => {
   return (
     <Stack spacing={2}>
       <Typography type="subtitle"> {title} </Typography>
-      {tasks.map((task) =>
-        <TaskItem key={task.id} task={task} onClick={onClick}/>
-      )}
+      {
+        tasks.length === 0 ?
+          <EmptyListMsg><Text size="15px" color="var(--black-light)">항목이 없습니다.</Text></EmptyListMsg>
+          :
+          tasks.map((task) =>
+            <TaskItem key={task.id} task={task} onClick={onClick}/>
+          )
+      }
     </Stack>
   );
 };
