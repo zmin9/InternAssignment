0eb5d86fcca6d49f56fe5873381d5d435c6fde05
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-28 11:58:16 +0900

    fix: #4 enable each left/right as well as side in Padding.jsx

--- 3/src/component/container/Padding.jsx
+++ 3/src/component/container/Padding.jsx
@@ -1,18 +1,24 @@
 import styled from 'styled-components';
 
 const PaddingBox = styled.div`
-  padding: ${({top, right, bottom, left}) => `${top} ${right} ${bottom} ${left}`};
+  padding: ${({ top, right, bottom, left }) => `${top} ${right} ${bottom} ${left}`};
 `;
 
-const Padding = ({top, right, bottom, left, side, children}) => {
-	return <PaddingBox
-		top={top || 0}
-		right={right || side || 0}
-		bottom={bottom || 0}
-		left={left || side || 0}
-	>
-		{children}
-	</PaddingBox>;
+const Padding = ({ top, right, bottom, left, side, children }) => {
+	const t = top === undefined? 0 : top;
+	const b = bottom === undefined? 0 : bottom;
+	const r = right === undefined? (side === undefined? 0: side) : right;
+	const l = left === undefined? (side === undefined? 0: side) : left;
+	return (
+		<PaddingBox
+			top={t}
+			right={r}
+			bottom={b}
+			left={l}
+		>
+			{children}
+		</PaddingBox>
+	);
 };
 
 export default Padding;
