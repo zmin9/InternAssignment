6c9303e98380ff595560e742dd99a92e7e7d15da
o3o0903@gmail.com <o3o0903@gmail.com>
2022-07-29 12:34:33 +0900

    refactor: #4 add nullish coalescing operator instead of conditional operator

--- 3/src/component/container/Padding.jsx
+++ 3/src/component/container/Padding.jsx
@@ -4,21 +4,15 @@ const PaddingBox = styled.div`
   padding: ${({ top, right, bottom, left }) => `${top} ${right} ${bottom} ${left}`};
 `;
 
-const Padding = ({ top, right, bottom, left, side, children }) => {
-	const t = top === undefined? 0 : top;
-	const b = bottom === undefined? 0 : bottom;
-	const r = right === undefined? (side === undefined? 0: side) : right;
-	const l = left === undefined? (side === undefined? 0: side) : left;
-	return (
-		<PaddingBox
-			top={t}
-			right={r}
-			bottom={b}
-			left={l}
-		>
-			{children}
-		</PaddingBox>
-	);
-};
+const Padding = ({ top, right, bottom, left, side, children }) => (
+	<PaddingBox
+		top={top ?? 0}
+		right={right ?? side ?? 0}
+		bottom={bottom ?? 0}
+		left={left ?? side ?? 0}
+	>
+		{children}
+	</PaddingBox>
+);
 
 export default Padding;
